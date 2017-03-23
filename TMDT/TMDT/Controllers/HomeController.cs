65f8587df49cf437fb418000041d76b7d73c8011
Thong <Thong@THONGMAP95>
2017-03-23 08:44:52 +0700

    tích hợp chatbox

--- TMDT/TMDT/Controllers/HomeController.cs
+++ TMDT/TMDT/Controllers/HomeController.cs
@@ -20,64 +20,7 @@ namespace TMDT.Controllers
 {
     public class HomeController : Controller
     {
-        private Uri RedirectUri
-        {
-            get
-            {
-                var uriBuilder = new UriBuilder(Request.Url);
-                uriBuilder.Query = null;
-                uriBuilder.Fragment = null;
-                uriBuilder.Path = Url.Action("FacebookCallback");
-                return uriBuilder.Uri;
-            }
-        }
-        [AllowAnonymous]
-        public ActionResult Facebook()
-        {
-            var fb = new FacebookClient();
-            var loginUrl = fb.GetLoginUrl(new
-            {
-                client_id = "444195149059600",
-                client_secret = "89223ca2d87cc4a741000d5c1ea57694",
-                redirect_uri = RedirectUri.AbsoluteUri,
-                response_type = "code",
-                scope = "email" // Add other permissions as needed
-            });
-
-            return Redirect(loginUrl.AbsoluteUri);
-        }
-
-        public ActionResult FacebookCallback(string code)
-        {
-            var fb = new FacebookClient();
-            dynamic result = fb.Post("oauth/access_token", new
-            {
-                client_id = "444195149059600",
-                client_secret = "89223ca2d87cc4a741000d5c1ea57694",
-                redirect_uri = RedirectUri.AbsoluteUri,
-                code = code
-            });
-
-            var accessToken = result.access_token;
-
-            // Store the access token in the session for farther use
-            Session["AccessToken"] = accessToken;
-
-            // update the facebook client with the access token so 
-            // we can make requests on behalf of the user
-            fb.AccessToken = accessToken;
-
-            // Get the user's information
-            dynamic me = fb.Get("me?fields=first_name,middle_name,last_name,id,email");
-            string email = me.email;
-            string firstname = me.first_name;
-            string middlename = me.middle_name;
-            string lastname = me.last_name;
-
-            // Set the auth cookie
-            FormsAuthentication.SetAuthCookie(email, false);
-            return RedirectToAction("Index", "Home");
-        }
+      
         public ActionResult Index()
         {
             ViewBag.New = new ProductDAO().ListNewProduct(12);
